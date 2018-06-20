ceb05f12cb1230951e98f1a4bc5a2d8c5cca10f3
opheelia <ophelia.rey@gmail.com>
2018-06-20 09:40:17 +0200

    Removing bugs

--- ui/controllers/NewtypeWindow.py
+++ ui/controllers/NewtypeWindow.py
@@ -35,6 +35,7 @@ class NewtypeWindow(QMainWindow, newtypeUI.Ui_MainWindow):
     def get_types(self):
         self.gif_label.setMovie(self.movie)
         self.movie.start()
+        self.Enregistrer.setDisabled(True)
         worker = Worker(self.api.get_streams)
         worker.signals.result.connect(self.get_types_received)
         self.threadpool.start(worker)
@@ -47,10 +48,16 @@ class NewtypeWindow(QMainWindow, newtypeUI.Ui_MainWindow):
         for stream in streams:
             types = types + str(stream) + "\n"
         self.types_affichage.setText(types)
+        self.Enregistrer.setEnabled(True)
 
     def create_type(self):
         new_type = str(self.nouveau_type.text())
-        if new_type:
+        try:
+            assert new_type is not ''
+            assert new_type not in self.types_affichage.toPlainText()
+        except AssertionError:
+            self.open_dialog("Ce type est déjà existant !")
+        else:
             self.gif_register.setMovie(self.movie)
             self.movie.start()
             worker = Worker(self.api.add_new_type, new_type)
@@ -62,6 +69,11 @@ class NewtypeWindow(QMainWindow, newtypeUI.Ui_MainWindow):
         self.movie.stop()
         self.gif_register.clear()
         self.open_register_window()
+
+    def open_dialog(self, message):
+        from ui.controllers.Dialog import Dialog
+        self.dialog = Dialog(message, parent=self)
+        self.dialog.show()
 
     def open_register_window(self):
         from ui.controllers.RegisterWindow import RegisterWindow
